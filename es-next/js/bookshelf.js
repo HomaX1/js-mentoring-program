58be63ff209cce535230424c21ac6b38bfa58aa9
Anastasiia_Baitasova <anastasiia_baitasova@epam.com>
2019-05-07 17:58:45 +0300

    added some minor fixes

--- es-next/js/bookshelf.js
+++ es-next/js/bookshelf.js
@@ -12,8 +12,8 @@ export default class BookShelf {
 
     showAllBooks() {
         for (let book of this.read(this.books)) {
-            let stories = document.getElementsByClassName('stories')[0];
-            let newStory = document.createElement('div');
+            const stories = document.querySelector('.stories');
+            const newStory = document.createElement('div');
 
             newStory.innerHTML = `<h2>${book.title}</h2><p>${book.template}</p>`;
             stories.appendChild(newStory);
@@ -22,15 +22,15 @@ export default class BookShelf {
     }
 }
 
-let story = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
+const story = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
 dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
 consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
 Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum`;
 
-let book1 = new Book(1, 'Nick', 18, story);
-let book2 = new Book(2, 'John', 25, story);
-let book3 = new Book(3, 'Mike', 12, story);
+const book1 = new Book(1, 'Nick', 18, story);
+const book2 = new Book(2, 'John', 25, story);
+const book3 = new Book(3, 'Mike', 12, story);
 
-let bookshelf = new BookShelf([book1, book2, book3]);
+const bookshelf = new BookShelf([book1, book2, book3]);
 
 bookshelf.showAllBooks();
--- es-next/js/xhr.js
+++ es-next/js/xhr.js
@@ -3,49 +3,47 @@ export default class Xhr {
     }
 
     get(url) {
-        return new Xhr()._request(url, {method: 'GET'});
+        return this._request(url, {method: 'GET'});
     }
 
-    post(url, data) {
-        data = JSON.stringify(data);
-        const options = Object.assign({method: 'POST'}, {body: data});
-        return new Xhr()._request(url, options);
-    }
-
-    put(url, data) {
-        data = JSON.stringify(data);
-        const options = Object.assign({method: 'PUT'}, {body: data});
-        return new Xhr()._request(url, options);
-    }
-
-    delete(url) {
-        return new Xhr()._request(url, {method: 'DELETE'});
-    }
+    // post(url, data) {
+    //     data = JSON.stringify(data);
+    //     const options = Object.assign({method: 'POST'}, {body: data});
+    //     return this._request(url, options);
+    // }
+    //
+    // put(url, data) {
+    //     data = JSON.stringify(data);
+    //     const options = Object.assign({method: 'PUT'}, {body: data});
+    //     return this._request(url, options);
+    // }
+    //
+    // delete(url) {
+    //     return this._request(url, {method: 'DELETE'});
+    // }
 
     async _request(url, options) {
         return await fetch(url, options)
-            .then((response) => {
-                return response.json();
-            })
+            .then((response) => response.json())
             .catch((error) => {
                 console.log('There has been a problem with your fetch operation: ' + error.message);
             });
     }
 }
 
-let xhr = new Xhr();
+const xhr = new Xhr();
 
 (async () => {
-    const getResult = await xhr.get('https://swapi.co/api/people/1');
+    const getResult = await xhr.get('https://swapi.co/api');
     console.log(getResult);
 
-    const postResult = await xhr.post('https://swapi.co/api/people/2', {name: 'Leia'});
-    console.log(postResult);
-
-    const putResult = await xhr.put('https://swapi.co/api/people/3', {name: 'Chui'});
-    console.log(putResult);
-
-    const deleteResult = await xhr.delete('https://swapi.co/api/people/4');
-    console.log(deleteResult);
+    // const postResult = await xhr.post('https://swapi.co/api/people/2', {name: 'Leia'});
+    // console.log(postResult);
+    //
+    // const putResult = await xhr.put('https://swapi.co/api/people/3', {name: 'Chui'});
+    // console.log(putResult);
+    //
+    // const deleteResult = await xhr.delete('https://swapi.co/api/people/4');
+    // console.log(deleteResult);
 })();
 
