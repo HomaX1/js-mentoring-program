cc528eb5fa873b6c65455cd8b0c8ad094d07d03c
Anastasiia_Baitasova <anastasiia_baitasova@epam.com>
2019-05-13 16:22:25 +0300

    proceeding work with Typescript home task

--- typescript/src/ts/abstract-classes/person.ts
+++ typescript/src/ts/abstract-classes/person.ts
@@ -2,15 +2,30 @@ import PeopleResultModel from '../interfaces/people-result.model';
 import PeopleModel from '../interfaces/people.model';
 
 export default abstract class Person implements PeopleResultModel {
-    name = '';
-    films = [];
 
-    public showData(obj: PeopleModel): void {
-        const result = this.filterData<PeopleModel>(obj);
-        console.log(result);
+    // protected constructor(
+    //     name = '',
+    //     films = []
+    // ){}
+    name: string;
+    films: Array<string>;
+    protected constructor(name: string, films: Array<string>) {
+        this.name = name;
+        this.films = films;
     }
 
-    private filterData<T>(obj: PeopleModel): PeopleResultModel {
-        return {name: obj.name, films: obj.films};
-    }
+    abstract get(url: string): object;
+    abstract async _request<T>(url: string, options: {}): Promise<T>;
+
+    abstract showData<T>(obj: PeopleModel): void;
+
+
+    // public showData(obj: PeopleModel): void {
+    //     const result = this.filterData<PeopleModel>(obj);
+    //     console.log(result);
+    // }
+    //
+    // private filterData<T>(obj: PeopleModel): PeopleResultModel {
+    //     return {name: obj.name, films: obj.films};
+    // }
 }
--- typescript/src/ts/request.ts
+++ typescript/src/ts/request.ts
@@ -1,12 +1,18 @@
 import Person from './abstract-classes/person';
+import PeopleResultModel from './interfaces/people-result.model';
+import PeopleModel from './interfaces/people.model';
 
 export default class Request<T> extends Person {
+
+    constructor(name: string, films: Array<string>) {
+        super(name, films);
+    }
 
     get(url: string) {
         return this._request(url, {method: 'GET'});
     }
 
-    async _request(url: string, options: {}): Promise<T> {
+    async _request<T>(url: string, options: {}): Promise<T> {
         return await fetch(url, options)
             .then((response) => {
                 return response.json();
@@ -14,5 +20,9 @@ export default class Request<T> extends Person {
             .catch((error) => {
                 console.log('There has been a problem with your fetch operation: ' + error.message);
             });
+    }
+
+    showData<T>(obj: PeopleModel): void {
+        console.log({name: obj.name, films: obj.films});
     }
 }
