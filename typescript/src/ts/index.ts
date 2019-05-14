30e0ee763daac0954abf5b860452581c33585032
Anastasiia Baitasova <baitasova.anastasiia@gmail.com>
2019-05-14 21:45:32 +0300

    done Typescript home task

--- typescript/src/ts/index.ts
+++ typescript/src/ts/index.ts
@@ -4,7 +4,6 @@ import PeopleModel from './interfaces/people.model';
 (async () => {
     const request = new Request<PeopleModel>();
     const result = await request.get('https://swapi.co/api/people/1');
-    console.log(result);
 
     request.showData(result);
 })();
--- typescript/src/ts/request.ts
+++ typescript/src/ts/request.ts
@@ -1,17 +1,14 @@
 import Person from './abstract-classes/person';
 import PeopleModel from './interfaces/people.model';
-import PeopleResultModel from './interfaces/people-result.model';
 
 export default class Request<T> extends Person {
     get(url: string) {
-        return this._request(url, {method: 'GET'});
+        return this._request<T>(url, {method: 'GET'});
     }
 
      async _request<T>(url: string, options: {}): Promise<T> {
         return await fetch(url, options)
-            .then((response) => {
-                return response.json();
-            })
+            .then((response) => response.json())
             .catch((error) => {
                 console.log('There has been a problem with your fetch operation: ' + error.message);
             });
